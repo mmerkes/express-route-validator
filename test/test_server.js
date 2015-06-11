a23d3de4de3402ec3a943e4eb3142eb4dd33aa40
Merkes <matt.merkes@gmail.com>
2015-06-11 13:44:33 -0700

    Refactors middleware.js, moves test server into standalone file, completes more integration tests

---
+++ test/test_server.js
@@ -0,0 +1,94 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    routeValidator = require('../lib/middleware'),
+    app = express();
+
+app.use(bodyParser.json())
+
+app.get('/items', routeValidator.validate({
+  query: {
+    since: { isDate: true },
+    limit: { isInt: true, isRequired: true },
+    page: { isInt: true, isRequired: false },
+    sort: { isRequired: true }
+  }
+}), function (req, res) {
+  return res.status(200).end();
+});
+
+app.get('/items/:item', routeValidator.validate({
+  params: {
+    item: { isMongoId: true, isRequired: true }
+  }
+}), function (req, res, next) {
+  return res.status(200).end();
+});
+
+app.post('/items', routeValidator.validate({
+  body: {
+    name: { isRequired: true },
+    date: { isRequired: true, isDate: true },
+    type: { isRequired: true, isIn: ['lawn', 'garden', 'tools'] },
+    user: { isRequired: true, isEmail: true },
+    uuid: { isRequired: false, isUUID: true },
+    url: { isURL: true }
+  }
+}), function (req, res) {
+  return res.status(200).end();
+});
+
+app.get('/items/:item/messages/:message', routeValidator.validate({
+  params: {
+    item: { isMongoId: true, isRequired: true },
+    message: { isMongoId: true, isRequired: true }
+  }
+}), function (req, res, next) {
+  return res.status(200).end();
+});
+
+app.get('/users', routeValidator.validate({
+  query: {
+    since: { isDate: true },
+    limit: { isInt: true, isRequired: true },
+    page: { isInt: true, isRequired: false },
+    sort: { isRequired: true }
+  },
+  callNext: true
+}), function (req, res) {
+  return res.status(200).end();
+});
+
+app.get('/users/:user', routeValidator.validate({
+  params: {
+    user: { isRequired: true, isEmail: true }
+  },
+  errorHandler: function (err, req, res, next) {
+    if (err) {
+      return res.status(400).send({
+        message: 'routeErrorHandler',
+        error: err.message
+      });
+    }
+    return res.status(500).send({
+      error: 'Don\'t know how we got here...'
+    });
+  }
+}), function (req, res) {
+  return res.status(200).end();
+});
+
+app.use( function (err, req, res, next) {
+  if (err) {
+    return res.status(400).send({
+      error: err.message,
+      message: 'calledNext'
+    });
+  }
+  return res.status(500).send({
+    error: 'Don\'t know how we got here...'
+  });
+});
+
+app.listen(3000);
+
+module.exports = app;
